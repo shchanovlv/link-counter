c634ad1688420144748196c1595c1c0777318c91
shchanovlv <shchanovlv@mail.ru>
2022-01-17 13:18:27 +0300

    init

---
+++ client/src/pages/LinksPage.js
@@ -0,0 +1,26 @@
+import React, { useCallback, useContext, useState, useEffect } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook'
+import { Loader } from '../components/Loader'
+import { LinksList } from '../components/LinksList'
+
+export const LinksPage = () => {
+  const { token } = useContext(AuthContext)
+  const { request, loading } = useHttp()
+  const [links, setLinks] = useState(null)
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request('/api/link', 'GET', null, {
+        Authorization: `Bearer ${token}`,
+      })
+      setLinks(fetched)
+    } catch (e) {}
+  }, [token, request])
+  useEffect(() => {
+    fetchLinks()
+  }, [fetchLinks])
+  if (loading) {
+    return <Loader />
+  }
+  return <>{!loading && links && <LinksList links={links} />}</>
+}
---
+++ middleware/auth.middleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+module.exports = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next()
+  }
+  try {
+    const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
+    if (!token) {
+      return res.status(401).json({ message: 'Нет авторизации' })
+    }
+    req.user = jwt.verify(token, config.get('jwtSecret'))
+    next()
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
+}
